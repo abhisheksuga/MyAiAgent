dce107f966552d2daf0da2a6a7cf932a11f1dbaa
Abhishek Suga <abhisheksuga1996@gmail.com>
2025-09-11 17:34:07 +0100

    added test cases for the calculator app and modified the main file to take prompt via CLI

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model="gemini-2.0-flash-001",contents="why should one consider learning when they already know python")
-if response.prompt_feedback and response.prompt_feedback.block_reason:
-    print(f"Response was blocked. Reason: {response.prompt_feedback.block_reason}")
-else:
+def main():
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) <2 :
+        print("A promt is needed ")
+        sys.exit(1)
+
+    verbose_flag = False 
+
+    if len(sys.argv) == 3 and sys.argv[2] =="--verbose" :
+        verbose_flag = True
+
+    prompt = sys.argv[1]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
+
+    if response is None or response.usage_metadata is None:
+        print("Response is malformed")
+        return 
+    
     print(response.text)
-    print(f"prompt_tokens:{response.usage_metadata.prompt_token_count}")
-    print(f"response_tokens:{response.usage_metadata.candidates_token_count}")
+
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
+
+main()
 
 
